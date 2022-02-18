019879bc0b6f039ae5876d69c5a1444abfc9d4d2
Yuan Xie <yuanxie@outlook.com>
2022-02-18 17:16:10 -0500

    use bottom tabs to do navigation

--- App.js
+++ App.js
@@ -1,18 +1,40 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from './components/HomeScreen';
 import DetailsScreen from './components/DetailsScreen';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import ProfileScreen from './components/ProfileScreen';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+            } else if (route.name === 'Profile') {
+              iconName = focused ? 'settings' : 'settings';
+            } else if (route.name === 'Details') {
+              iconName = focused ? 'heart' : 'heart';
+            }
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+        <Tab.Screen name="Details" component={DetailsScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
