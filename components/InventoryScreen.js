ecca575ec6672cdb726a425c14780ce178f37b62
Yuan Xie <yuanxie@outlook.com>
2022-02-24 15:36:19 -0500

    add recipe details

--- components/InventoryScreen.js
+++ components/InventoryScreen.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {Button, View, Text, ActivityIndicator, StyleSheet} from 'react-native';
-import { IngredientInformation } from './IngredientInformation';
+import {IngredientInformation} from './IngredientInformation';
 import styled from 'styled-components/native';
 const TEST = [{}, {}, {}];
 
@@ -20,7 +20,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginLeft: 18,
   },
-})
+});
 
 export default class InventoryScreen extends Component {
   constructor(props) {
@@ -28,72 +28,73 @@ export default class InventoryScreen extends Component {
 
     this.state = {
       data: [],
-      isLoading: true
+      isLoading: true,
     };
   }
 
-
   getIngredients = async () => {
     try {
-      this.setState({ isLoading: true});
+      this.setState({isLoading: true});
       const response = await fetch('http:localhost:4000/storage/ingredients', {
         headers: {
-          'Authorization': `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY0NTY0NTIwMywianRpIjoiZTMyYjA1OGEtNmIwMC00ZTJhLThjYTktYTJjMjI1Y2RkNjdiIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6eyJfaWQiOiI2MWQyZjViODY0MWJmNTk1YzI0MjA5MjkiLCJmaXJzdE5hbWUiOiJGdWhhaSIsImxhc3ROYW1lIjoiR2FvIiwiZW1haWwiOiJmaGFpLmdhb0BnbWFpbC5jb20iLCJwYXNzd29yZCI6InRlc3QifSwibmJmIjoxNjQ1NjQ1MjAzLCJleHAiOjE2NDU3MzE2MDN9.vghDhBfLoBOb5WAKK7ufD5Fx88sgLKqWC2dgo2oTjxI`,
-          'page': '1'
-        }
+          Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY0NTY0NTIwMywianRpIjoiZTMyYjA1OGEtNmIwMC00ZTJhLThjYTktYTJjMjI1Y2RkNjdiIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6eyJfaWQiOiI2MWQyZjViODY0MWJmNTk1YzI0MjA5MjkiLCJmaXJzdE5hbWUiOiJGdWhhaSIsImxhc3ROYW1lIjoiR2FvIiwiZW1haWwiOiJmaGFpLmdhb0BnbWFpbC5jb20iLCJwYXNzd29yZCI6InRlc3QifSwibmJmIjoxNjQ1NjQ1MjAzLCJleHAiOjE2NDU3MzE2MDN9.vghDhBfLoBOb5WAKK7ufD5Fx88sgLKqWC2dgo2oTjxI`,
+          page: '1',
+        },
       });
       const json = await response.json();
-      this.setState({data: json.message})
+      this.setState({data: json.message});
     } catch (error) {
-      console.log(error.message)
+      console.log(error.message);
     } finally {
-      this.setState({ isLoading: false});
+      this.setState({isLoading: false});
     }
-  }
+  };
 
   componentDidMount() {
     this.getIngredients();
   }
 
-
   render() {
     const {data, isLoading} = this.state;
 
-
     return (
       <View style={styles.container}>
-        {isLoading ? <ActivityIndicator/> : (
-            <View style={styles.appArea}>
-              <Text style={styles.title}>Ingredient Information</Text>
-              <View>
-                {data.map((t, i) => {
-                    return <IngredientInformation 
-                    key={i} data={t}
+        {isLoading ? (
+          <ActivityIndicator />
+        ) : (
+          <View style={styles.appArea}>
+            <Text style={styles.title}>Ingredient Information</Text>
+            <View>
+              {data.map((t, i) => {
+                return (
+                  <IngredientInformation
+                    key={i}
+                    data={t}
                     onChange={this.getIngredients}
-                    />
-                })}
-              </View>
+                  />
+                );
+              })}
             </View>
-
+          </View>
         )}
       </View>
-    )
+    );
   }
 }
 
 // function MenuScreen({navigation}) {
 //   return (
-    
+
 //     // <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
 //     //   <Text>Profile Screen</Text>
 //     //   <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
 //     // </View>
 //     <>
 //     <View>
-//       <Text 
+//       <Text
 //       // style={styles.title}
 //       >Ingredient Information</Text>
-      
+
 //       </View>
 //       <Container>
 //           {TEST.map((t, i) => {
